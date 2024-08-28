141ef8bd4a9de42da75c3d2efc104c6a57b07ea3
Iván Urteaga <ivan.urteaga.26@gmail.com>
2024-08-28 16:17:33 +0000

    added int_sales_margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,12 @@
+select 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64),
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+from {{ref("stg_raw__sales")}} s
+left join {{ref("stg_raw__product")}} p
+ using (products_id)
