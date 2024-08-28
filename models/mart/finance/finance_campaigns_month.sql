ed88407df7218438bc0116975a00e7f56f4193a5
Iván Urteaga <ivan.urteaga.26@gmail.com>
2024-08-28 22:13:20 +0000

    fixed finance/finance_campaign_month

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,10 +1,8 @@
 SELECT 
-    DATE_TRUNC(c.date_date, MONTH) AS month,  
-    ROUND(SUM(f.operational_margin - c.ads_cost), 2) AS ads_margin,
-    ROUND(SUM(f.average_basket), 2) AS average_basket,
-    ROUND(SUM(f.operational_margin), 2) AS operational_margin
-FROM {{ ref('int_campaigns_day') }} c
-JOIN {{ ref('finance_days') }} f
-    ON c.date_date = f.date_date 
+    DATE_TRUNC(date_date, MONTH) AS month,  
+    ROUND(SUM(ads_margin),2) AS ads_margin,
+    ROUND(SUM(average_basket),2) AS average_basket,
+    ROUND(SUM(operational_margin), 2) AS operational_margin
+FROM {{ ref('finance_campaigns_day') }} 
 GROUP BY month
 ORDER BY month DESC
