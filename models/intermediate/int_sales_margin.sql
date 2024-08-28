8ae9a36cc8ad80496eb35a2b297e5fe3d0237f7d
Iván Urteaga <ivan.urteaga.26@gmail.com>
2024-08-28 16:20:35 +0000

    fixed sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,7 @@ select
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64),
+    CAST(purchase_price AS FLOAT64) as purchase_price,
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 from {{ref("stg_raw__sales")}} s
