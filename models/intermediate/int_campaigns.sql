bacc71d4734c142243afaecd25b16fb1a5315b74
Iván Urteaga <ivan.urteaga.26@gmail.com>
2024-08-30 00:59:10 +0000

    added dependencies

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,7 +1,12 @@
-select * from {{ref("stg_raw__adwords")}}
+{# select * from {{ref("stg_raw__adwords")}}
 union all
 select * from {{ref("stg_raw__bing")}}
 union all
 select * from {{ref("stg_raw__facebook")}}
 union all
-select * from {{ref("stg_raw__criteo")}}
+select * from {{ref("stg_raw__criteo")}} #}
+
+{{
+ dbt_utils.union_relations(
+ relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
+ )}}
